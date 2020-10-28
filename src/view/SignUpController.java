0fdabf536cf2fde8431f000170d9e932e900a3ea
aitor <aitor@AITORFI-DESKTOP>
2020-10-28 17:29:27 +0100

    More specific error messages implemented.

--- src/view/SignUpController.java
+++ src/view/SignUpController.java
@@ -31,29 +31,31 @@ import user.User;
 
 /**
  * Class that controls the SignUpWindow behaviour.
+ *
  * @author aitor
  */
 public class SignUpController {
+
     @FXML
     private Stage stage;
-    
+
     @FXML
     private Button btnLogin;
     @FXML
     private Button btnAccept;
-    
+
     @FXML
     private PasswordField pwdPassword;
     @FXML
     private PasswordField pwdConfirmPassword;
-    
+
     @FXML
     private TextField txtName;
     @FXML
     private TextField txtEmail;
     @FXML
     private TextField txtUsername;
-    
+
     @FXML
     private Label lblErrorUsername;
     @FXML
@@ -64,16 +66,14 @@ public class SignUpController {
     private Label lblErrorConfirmPassword;
     @FXML
     private Label lblErrorName;
-    
+
     //Used to handle textField input errors.
     HashMap<String, Boolean> textFieldErrors = new HashMap<>();
-    
-    
-    
-    
+
     /**
      * Initializes the scene and its components
-     * @param root 
+     *
+     * @param root
      */
     public void initStage(Parent root) {
         Scene scene = new Scene(root);
@@ -81,7 +81,7 @@ public class SignUpController {
         stage.setTitle("Sign Up");
         stage.setResizable(false);
         stage.setOnShowing(this::handleWindowShowing);
-        
+
         txtUsername.textProperty().addListener(this::handleTextChangeUsername);
         textFieldErrors.put("txtUsernameError", true);
         txtEmail.textProperty().addListener(this::handleTextChangeEmail);
@@ -92,49 +92,58 @@ public class SignUpController {
         textFieldErrors.put("pwdPasswordError", true);
         pwdConfirmPassword.textProperty().addListener(this::handleTextChangeConfirmPassword);
         textFieldErrors.put("pwdConfirmPasswordError", true);
-        
-        stage.show();
-    }
-    
-    public void initialize(){
-        btnAccept.setDisable(true);
+
         lblErrorConfirmPassword.setVisible(false);
         lblErrorEmail.setVisible(false);
         lblErrorName.setVisible(false);
         lblErrorUsername.setVisible(false);
         lblErrorPassword.setVisible(false);
+
+        btnAccept.setDisable(true);
         btnAccept.setTooltip(
                 new Tooltip("Pulse para validar credenciales"));
-        
+
+        stage.show();
     }
-    
+
     /**
      * Handles the OnShowing event of the stage.
-     * @param event 
+     *
+     * @param event
      */
-    private void handleWindowShowing(WindowEvent event){
+    private void handleWindowShowing(WindowEvent event) {
         btnAccept.setDisable(true);
-        
+
         txtUsername.requestFocus();
-        
+
         lblErrorConfirmPassword.setVisible(false);
         lblErrorEmail.setVisible(false);
         lblErrorName.setVisible(false);
         lblErrorPassword.setVisible(false);
         lblErrorUsername.setVisible(false);
     }
-    
+
+    /**
+     * Switches to the LogIn window.
+     *
+     * @param event
+     */
     @FXML
     public void handleButtonLogIn(ActionEvent event) {
-        try{
-            start_logIn(stage);
-        } catch (IOException e){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogInWindow.fxml"));
+            Parent root = (Parent) loader.load();
+
+            LogInController controller = (loader.getController());
+            controller.setStage(stage);
+            controller.initStage(root);
+        } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Could not change to Log In window.", ButtonType.OK);
             alert.showAndWait();
         }
-        
+
     }
-    
+
     @FXML
     public void handleButtonAccept(ActionEvent event) {
         User user = new User();
@@ -144,176 +153,158 @@ public class SignUpController {
         user.setPassword(pwdPassword.getText());
         user.setLastAccess(Date.valueOf(LocalDate.now()));
         SignableFactory.getSignable().signIn(user);
-        
+
     }
-    
+
     /**
      * Tests if there is any error on every txtUsername text propertie change.
-     * @param obs 
+     * @param obs
      */
-    private void handleTextChangeUsername(Observable obs){
+    private void handleTextChangeUsername(Observable obs) {
         Integer txtUsernameLength = txtUsername.getText().trim().length();
-        
-        //Testing the field length...
-        if(txtUsernameLength == 0 || txtUsernameLength > 255){
-            lblErrorUsername.setVisible(true);
+
+        //If there is any error...
+        if (txtUsernameLength == 0 || txtUsernameLength > 255) {
+            //Sets the error message when the fiel is empty.
+            if (txtUsernameLength == 0) lblErrorUsername.setText("* Field must not be empty");
+            //Sets the error message when the field is longer than 255 characters.
+            if (txtUsernameLength > 255) lblErrorUsername.setText("* Must be less than 255 characters");
+            
             textFieldErrors.put("txtUsernameError", true);
+            lblErrorUsername.setVisible(true);
         } else {
-            lblErrorUsername.setVisible(false);
             textFieldErrors.put("txtUsernameError", false);
+            lblErrorUsername.setVisible(false);
         }
-        
+
         //Tests if there is any input error and disables btnAccept if so.
         testInputErrors();
     }
-    
+
     /**
-     * Tests if there is any error on every txtEmail text propertie change.
-     * @param obs 
-     */
-    private void handleTextChangeEmail(Observable obs){
-        Integer txtEmailLength = txtEmail.getText().trim().length();
-        Pattern patternEmail = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@" +
-                "[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        Matcher matcherEmail = patternEmail.matcher(txtEmail.getText());
-        
-        //Testing if it has the correct format...
-        if(!matcherEmail.matches()){
-            lblErrorEmail.setText("* Must match the pattern example@example.com");
-            textFieldErrors.put("txtEmailError", true);
-        } else {
-            textFieldErrors.put("txtEmailError", false);
-        }
-        
-        //Testing the field length
-        if(txtEmailLength > 255 || txtEmailLength == 0){
-            lblErrorEmail.setText("* Must be between 0 and 255 characters");
-            textFieldErrors.put("txtEmailError", true);
-        }
-        
-        //Setting visible the error label if there is any error
-        if(textFieldErrors.get("txtEmailError")){
-            lblErrorEmail.setVisible(true);
-        } else {
-            lblErrorEmail.setVisible(false);
-        }
-        
-        //Tests if there is any input error and disables btnAccept if so.
-        testInputErrors();
-    }
-    
-    /**
-     * Tests if there is any error on every txtName text propertie change.
-     * @param obs 
-     */
-    private void handleTextChangeName(Observable obs){
-        Integer txtNameLength = txtName.getText().trim().length();
-        
-        //Testing the field length and if it has the correct format...
-        Pattern patternName = Pattern.compile("^([A-Za-z]+[ ]?)+$");
-        Matcher matcherName = patternName.matcher(txtName.getText());
-        if(!matcherName.matches()){
-            lblErrorName.setText("* Must only contain letters");
-            textFieldErrors.put("txtNameError", true);
-        } else {
-            textFieldErrors.put("txtNameError", false);
-        }
-        
-        if(txtNameLength > 255 || txtNameLength == 0){
-            lblErrorName.setText("* Must be between 0 and 255 characters");
-            textFieldErrors.put("txtNameError", true);
-        }
-        
-        if(textFieldErrors.get("txtNameError")){
-            lblErrorName.setVisible(true);
-        } else {
-            lblErrorName.setVisible(false);
-        }
-        
-        //Tests if there is any input error and disables btnAccept if so.
-        testInputErrors();
-    }
-    
-    /**
-     * Tests if there is any error on every pwdPassword text propertie change.
-     * @param obs 
-     */
-    private void handleTextChangePassword(Observable obs){
-        Integer pwdPasswordLength = pwdPassword.getText().trim().length();
-        Integer pwdConfirmPasswordLength = pwdConfirmPassword.getText().trim().length();
-        
-        //Testing the lenght of the field...
-        if(pwdPasswordLength < 6 || pwdPasswordLength > 255){
-            lblErrorPassword.setVisible(true);
-            textFieldErrors.put("pwdPasswordError", true);
-        } else {
-            lblErrorPassword.setVisible(false);
-            textFieldErrors.put("pwdPasswordError", false);
-        }
-        
-        //Testing if the field matches with pwdConfirmPassword...
-        if(!pwdConfirmPassword.getText().equals(pwdPassword.getText())){
-            lblErrorConfirmPassword.setVisible(true);
-            textFieldErrors.put("pwdConfirmPasswordError", true);
-        } else {
-            lblErrorConfirmPassword.setVisible(false);
-            textFieldErrors.put("pwdConfirmPasswordError", false);
-        }
-        
-        //Tests if there is any input error and disables btnAccept if so.
-        testInputErrors();
-    }
-    
-    /**
-     * Tests if there is any error on every pwdConfirmPassword text propertie change.
-     * @param obs 
-     */
-    private void handleTextChangeConfirmPassword(Observable obs){
-        Integer pwdConfirmPasswordLength = pwdConfirmPassword.getText().trim().length();
-        
-        //Testing the lenght of the field and if it matches with pwdPassword...
-        if(!pwdConfirmPassword.getText().equals(pwdPassword.getText())){
-            lblErrorConfirmPassword.setVisible(true);
-            textFieldErrors.put("pwdConfirmPasswordError", true);
-        } else {
-            lblErrorConfirmPassword.setVisible(false);
-            textFieldErrors.put("pwdConfirmPasswordError", false);
-        }
-        
-        //Tests if there is any input error and disables btnAccept if so.
-        testInputErrors();
-    }
-    
-    /**
-     * Tests if there is any input error and disables btnAccept if so.
+     * Checks if there is any input error and disables btnAccept if so.
      */
     private void testInputErrors() {
-        if(textFieldErrors.get("txtUsernameError") || textFieldErrors.get("txtEmailError") ||
-                textFieldErrors.get("txtNameError") || textFieldErrors.get("pwdPasswordError") ||
-                textFieldErrors.get("pwdConfirmPasswordError")){
+        //Checks if there is any input errors... 
+        if (textFieldErrors.get("txtUsernameError") || textFieldErrors.get("txtEmailError")
+                || textFieldErrors.get("txtNameError") || textFieldErrors.get("pwdPasswordError")
+                || textFieldErrors.get("pwdConfirmPasswordError")) {
 
-            btnAccept.setDisable(true);  
+            btnAccept.setDisable(true);
         } else {
             btnAccept.setDisable(false);
         }
     }
-    
-    
-    public Stage getStage(){
+
+    /**
+     * Tests if there is any error on every txtEmail text propertie change.
+     * @param obs
+     */
+    private void handleTextChangeEmail(Observable obs) {
+        Integer txtEmailLength = txtEmail.getText().trim().length();
+        Pattern patternEmail = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@"
+                + "[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matcherEmail = patternEmail.matcher(txtEmail.getText());
+
+        //If there is any error...
+        if (txtEmailLength == 0 || txtEmailLength > 255 || !matcherEmail.matches()) {
+            //Sets the error message when the field does not match the pattern.
+            if (!matcherEmail.matches()) lblErrorEmail.setText("* Must match the pattern example@example.com");
+            //Sets the error message when the fiel is empty.
+            if (txtEmailLength == 0) lblErrorEmail.setText("* Field must not be empty");
+            //Sets the error message when the field is longer than 255 characters.
+            if (txtEmailLength > 255) lblErrorEmail.setText("* Must be less than 255 characters");
+            
+            textFieldErrors.put("txtEmailError", true);
+            lblErrorEmail.setVisible(true);
+        } else {
+            textFieldErrors.put("txtEmailError", false);
+            lblErrorEmail.setVisible(false);
+        }
+
+        //Tests if there is any input error and disables btnAccept if so.
+        testInputErrors();
+    }
+
+    /**
+     * Tests if there is any error on every txtName text propertie change.
+     * @param obs
+     */
+    private void handleTextChangeName(Observable obs) {
+        Integer txtNameLength = txtName.getText().trim().length();
+        Pattern patternName = Pattern.compile("^([A-Za-z]+[ ]?)+$");
+        Matcher matcherName = patternName.matcher(txtName.getText());
+
+        //If there is any error...
+        if (txtNameLength == 0 || txtNameLength > 255 || !matcherName.matches()) {
+            //Sets the error message when the field does not match the pattern.
+            if (!matcherName.matches()) lblErrorName.setText("* Must only contain letters");
+            //Sets the error message when the fiel is empty.
+            if (txtNameLength == 0) lblErrorName.setText("* Field must not be empty");
+            //Sets the error message when the field is longer than 255 characters.
+            if (txtNameLength > 255) lblErrorName.setText("* Must be less than 255 characters");
+            textFieldErrors.put("txtNameError", true);
+            lblErrorName.setVisible(true);
+        } else {
+            textFieldErrors.put("txtNameError", false);
+            lblErrorName.setVisible(false);
+        }
+
+        //Tests if there is any input error and disables btnAccept if so.
+        testInputErrors();
+    }
+
+    /**
+     * Tests if there is any error on every pwdPassword text propertie change.
+     * @param obs
+     */
+    private void handleTextChangePassword(Observable obs) {
+        Integer pwdPasswordLength = pwdPassword.getText().trim().length();
+
+        //If there is any error with pwdPassword...
+        if (pwdPasswordLength < 6 || pwdPasswordLength > 255) {
+            //Sets the error message when the fiel is shorter than 6 characters.
+            if (pwdPasswordLength < 6) lblErrorPassword.setText("* Must be at least 6 characters");
+            //Sets the error message when the field is longer than 255 characters.
+            if (pwdPasswordLength > 255) lblErrorPassword.setText("* Must be less than 255 characters");
+            
+            textFieldErrors.put("pwdPasswordError", true);
+            lblErrorPassword.setVisible(true);
+        } else {
+            textFieldErrors.put("pwdPasswordError", false);
+            lblErrorPassword.setVisible(false);
+        }
+
+        //If pwdConfirmPassword does not match with pwdPassword...
+        
+
+        //Tests if there is any input error and disables btnAccept if so.
+        testInputErrors();
+    }
+
+    /**
+     * Tests if there is any error on every pwdConfirmPassword text propertie change.
+     * @param obs
+     */
+    private void handleTextChangeConfirmPassword(Observable obs) {
+        //If pwdConfirmPassword does not match with pwdPassword...
+        if (!pwdConfirmPassword.getText().equals(pwdPassword.getText())) {
+            textFieldErrors.put("pwdConfirmPasswordError", true);
+            lblErrorConfirmPassword.setVisible(true);
+        } else {
+            textFieldErrors.put("pwdConfirmPasswordError", false);
+            lblErrorConfirmPassword.setVisible(false);
+        }
+
+        //Tests if there is any input error and disables btnAccept if so.
+        testInputErrors();
+    }
+
+    public Stage getStage() {
         return stage;
     }
-    
+
     public void setStage(Stage primaryStage) {
         stage = primaryStage;
     }
-
-    private void start_logIn(Stage primaryStage) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogInWindow.fxml"));
-        Parent root =(Parent)loader.load();
-        
-        LogInController controller = (loader.getController());
-        controller.setStage(primaryStage);
-        controller.initStage(root);
-    }
-    
 }
