5e8e39a9a944d59da818f12a52d7ed48b91496a6
2dam <2dam@HZ359575.TartangaLH.eus>
2020-10-29 13:41:48 +0100

    Test clases implemented

--- test/ui/controller/LoginControllerTest.java
+++ test/ui/controller/LoginControllerTest.java
@@ -5,41 +5,112 @@
  */
 package ui.controller;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
+import application.ClientApplication;
+import java.util.concurrent.TimeoutException;
+import javafx.stage.Stage;
 import org.junit.BeforeClass;
+import org.testfx.framework.junit.ApplicationTest;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.matcher.base.NodeMatchers.isFocused;
+import static org.testfx.matcher.base.NodeMatchers.isInvisible;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
 
 /**
+ * Testing class for Login view and controller. Tests login view behavior using
+ * TestFX framework.
  *
  * @author Lorena
  */
-public class LoginControllerTest{
-    
-    public LoginControllerTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LoginControllerTest extends ApplicationTest {
+
+    private static final String OVERSIZED_TEXT = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            + "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+    /**
+     * Starts application to be tested.
+     *
+     * @param stage Primary Stage object
+     * @throws Exception if we have any error
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        new ClientApplication().start(stage);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    /**
+     * Test the login view.
+     */
+    @Test
+    public void test1_InitialState() {
+        verifyThat("#txtUsername", isEnabled());
+        verifyThat("#pwdPassword", isEnabled());
+        verifyThat("#btnAccept", isDisabled());
+        verifyThat("#lblErrorLogin", isInvisible());
+        verifyThat("#lblErrorPassword", isInvisible());
+        verifyThat("#txtUsername", isFocused());
+
+    }
+
+    @Test
+    public void test2_TxtUsuarioMaximumLength() {
+        doubleClickOn("#txtUsername");
+        write(OVERSIZED_TEXT);
+        verifyThat("#btnAccept", isDisabled());
+        verifyThat("* Must be less than 255 characters", isVisible());
+    }
+
+    @Test
+    public void test3_PwdPasswordMaximumLength() {
+        doubleClickOn("#pwdPassword");
+        write(OVERSIZED_TEXT);
+        verifyThat("#btnAccept", isDisabled());
+        verifyThat("* Must be less than 255 characters", isVisible());
+    }
+
+    @Test
+    public void test4_TxtUsuarioNotEmpty() {
+        doubleClickOn("#txtUsername");
+        write("usuario1234");
+        verifyThat("#btnAccept", isDisabled());
+        eraseText(11);
+        verifyThat("* Field must not be empty", isVisible());
+    }
+
+    @Test
+    public void test5_TxtUsuarioIsCorrect() {
+        doubleClickOn("#txtUsername");
+        write("usuario1234");
+        doubleClickOn("#pwdPassword");
+        write("usuario1234");
+        verifyThat("#btnAccept", isEnabled());
+    }
+
+    @Test
+    public void test6_PwdPasswordMinimumLength() {
+        doubleClickOn("#pwdPassword");
+        write("123");
+        verifyThat("#btnAccept", isDisabled());
+        verifyThat("* Must be at least 6 characters", isVisible());
+    }
+
+    @Test
+    public void test7_PwdPasswordIsCorrect() {
+        doubleClickOn("#txtUsername");
+        write("usuario1234");
+        doubleClickOn("#pwdPassword");
+        write("123456");
+        verifyThat("#btnAccept", isEnabled());
+    }
 }
