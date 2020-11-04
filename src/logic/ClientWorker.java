1e3f641518ce968f1965e5b9b6308446b5863938
Martin Angulo <marlock97@gmail.com>
2020-11-04 14:20:11 +0100

    Started change to client thread.

---
+++ src/logic/ClientWorker.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import exceptions.EmailAlreadyExistsException;
+import exceptions.PasswordDoesNotMatchException;
+import exceptions.UnexpectedErrorException;
+import exceptions.UserAlreadyExistsException;
+import exceptions.UserNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import message.Message;
+import user.User;
+
+/**
+ * Handles messages from the server.
+ * @author Martin Angulo
+ */
+public class ClientWorker extends Thread {
+    /** Client socket that connects to the servers socket. */
+    private Socket clientSocket = null;
+    /** Input stream to receive objects from the server. */
+    private ObjectInputStream serverInput = null;
+    /** Output stream to send objects to the server. */
+    private ObjectOutputStream clientOutput = null;
+    
+    /**
+     * ClientWorker constructor, connects to the server and initializes IO.
+     */
+    public ClientWorker() {
+        try {
+            //Getting the client-server communication properties.
+            ResourceBundle configFile = ResourceBundle.getBundle("configuration.config");
+            Integer port = Integer.valueOf(configFile.getString("Port"));
+            String serverHost = configFile.getString("ServerHost");
+            //Initializing the client-server communication.
+            clientSocket = new Socket(serverHost, port);
+            serverInput = new ObjectInputStream(clientSocket.getInputStream());
+            clientOutput = new ObjectOutputStream(clientSocket.getOutputStream());
+        } catch (UnknownHostException ex) {
+            System.out.println("UnknownHostException: " + ex.getMessage());
+        } catch (IOException ie) {
+            System.out.println("IOException: " + ie.getMessage());
+        }
+    }
+    
+    /**
+     * Helper method to send messages to the server and receive responses.
+     * @param message Message to send. 
+     * @return Server response.
+     */
+    private Message sendMessage(Message message) {
+        Message serverResponse = null;
+        try {
+            //Send message to server
+            clientOutput.writeObject(message);
+            clientOutput.flush();
+            //Receive response
+            serverResponse = (Message)serverInput.readObject();
+            
+        } catch (IOException ex) {
+            System.out.println("IOException: " + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException: " + ex.getMessage());
+        }finally{
+            disconnect();
+        }
+        return serverResponse;
+    }
+    
+    /**
+     * Helper method to disconnect the IO and Socket from the server.
+     */
+    private void disconnect() {
+        try {
+            if(clientOutput != null)
+                clientOutput.close();
+            if(serverInput != null)
+                serverInput.close();
+            if (clientSocket != null)
+                clientSocket.close();
+        } catch(IOException ie) {
+            System.out.println("Socket Close Error: " + ie.getMessage());
+        }
+    }
+    
+    /**
+     * Sign up function implementation.
+     * @param user User to sign up.
+     * @return The User in case the application needs it.
+     * @throws UserAlreadyExistsException If the username is found in the DB.
+     * @throws EmailAlreadyExistsException If the email is found in the DB.
+     * @throws exceptions.UnexpectedErrorException If anything else goes wrong.
+     */
+    public synchronized User signUp(User user) throws UserAlreadyExistsException, EmailAlreadyExistsException, UnexpectedErrorException {
+        Message serverResponse = sendMessage(new Message(Message.Type.SIGN_UP, user));
+        switch(serverResponse.getType()) {
+        case SIGN_UP:
+            User signUpUser = (User)serverResponse.getData();
+            return signUpUser;
+        case USER_ALREADY_EXISTS:
+            throw new UserAlreadyExistsException(user.getLogin());
+        case EMAIL_ALREADY_EXISTS:
+            throw new EmailAlreadyExistsException(user.getEmail());
+        default:
+            throw new UnexpectedErrorException();
+        }
+    }
+
+    /**
+     * Sign in function implementation.
+     * @param user User to sign in.
+     * @return The User in case the application needs it.
+     * @throws exceptions.UserNotFoundException If the username is not found in the DB.
+     * @throws exceptions.PasswordDoesNotMatchException If the password does not 
+     * match with the username in the DB.
+     * @throws exceptions.UnexpectedErrorException If anything else goes wrong.
+     */
+    public synchronized User signIn(User user) throws UserNotFoundException, PasswordDoesNotMatchException, UnexpectedErrorException{
+        Message serverResponse = sendMessage(new Message(Message.Type.SIGN_IN, user));
+        switch(serverResponse.getType()) {
+        case SIGN_IN:
+            User logInUser = (User)serverResponse.getData();
+            return logInUser;
+        case USER_NOT_FOUND:
+            throw new UserNotFoundException(user.getLogin());
+        case PASSWORD_DOES_NOT_MATCH:
+            throw new PasswordDoesNotMatchException();
+        default:
+            throw new UnexpectedErrorException();
+        }
+    }
+    
+    /*
+     * Handles messages from the server.
+     */
+    @Override
+    public void run() {
+        
+    }
+}
--- src/logic/Signer.java
+++ src/logic/Signer.java
@@ -5,133 +5,23 @@ import exceptions.PasswordDoesNotMatchException;
 import exceptions.UnexpectedErrorException;
 import exceptions.UserAlreadyExistsException;
 import exceptions.UserNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ResourceBundle;
-import message.Message;
 import user.User;
+
 
 /**
  * Handles messages from the server.
  * @author Martin Angulo
  */
-public class Signer implements Signable{
-    /** Client socket that connects to the servers socket. */
-    private Socket clientSocket = null;
-    /** Input stream to receive objects from the server. */
-    private ObjectInputStream serverInput = null;
-    /** Output stream to send objects to the server. */
-    private ObjectOutputStream clientOutput = null;
+public class Signer extends Thread implements Signable{
+    ClientWorker worker = new ClientWorker();
     
-    /**
-     * Signer constructor, connects to the server and initializes IO.
-     */
-    public Signer() {
-        try {
-            //Getting the client-server communication properties.
-            ResourceBundle configFile = ResourceBundle.getBundle("configuration.config");
-            Integer port = Integer.valueOf(configFile.getString("Port"));
-            String serverHost = configFile.getString("ServerHost");
-            //Initializing the client-server communication.
-            clientSocket = new Socket(serverHost, port);
-            serverInput = new ObjectInputStream(clientSocket.getInputStream());
-            clientOutput = new ObjectOutputStream(clientSocket.getOutputStream());
-        } catch (UnknownHostException ex) {
-            System.out.println("UnknownHostException: " + ex.getMessage());
-        } catch (IOException ie) {
-            System.out.println("IOException: " + ie.getMessage());
-        }
+    @Override
+    public User signUp(User user) throws UserAlreadyExistsException, EmailAlreadyExistsException, UnexpectedErrorException {
+
     }
 
-    /**
-     * Helper method to send messages to the server and receive responses.
-     * @param message Message to send. 
-     * @return Server response.
-     */
-    private Message sendMessage(Message message) {
-        Message serverResponse = null;
-        try {
-            //Send message to server
-            clientOutput.writeObject(message);
-            clientOutput.flush();
-            //Receive response
-            serverResponse = (Message)serverInput.readObject();
-            
-        } catch (IOException ex) {
-            System.out.println("IOException: " + ex.getMessage());
-        } catch (ClassNotFoundException ex) {
-            System.out.println("ClassNotFoundException: " + ex.getMessage());
-        }finally{
-            disconnect();
-        }
-        return serverResponse;
-    }
-    
-    /**
-     * Helper method to disconnect the IO and Socket from the server.
-     */
-    private void disconnect() {
-        try {
-            if(clientOutput != null)
-                clientOutput.close();
-            if(serverInput != null)
-                serverInput.close();
-            if (clientSocket != null)
-                clientSocket.close();
-        } catch(IOException ie) {
-            System.out.println("Socket Close Error: " + ie.getMessage());
-        }
-    }
-    
-    /**
-     * Sign up function implementation.
-     * @param user User to sign up.
-     * @return The User in case the application needs it.
-     * @throws UserAlreadyExistsException If the username is found in the DB.
-     * @throws EmailAlreadyExistsException If the email is found in the DB.
-     * @throws exceptions.UnexpectedErrorException If anything else goes wrong.
-     */
     @Override
-    public synchronized User signUp(User user) throws UserAlreadyExistsException, EmailAlreadyExistsException, UnexpectedErrorException {
-        Message serverResponse = sendMessage(new Message(Message.Type.SIGN_UP, user));
-        switch(serverResponse.getType()) {
-        case SIGN_UP:
-            User signUpUser = (User)serverResponse.getData();
-            return signUpUser;
-        case USER_ALREADY_EXISTS:
-            throw new UserAlreadyExistsException(user.getLogin());
-        case EMAIL_ALREADY_EXISTS:
-            throw new EmailAlreadyExistsException(user.getEmail());
-        default:
-            throw new UnexpectedErrorException();
-        }
-    }
-
-    /**
-     * Sign in function implementation.
-     * @param user User to sign in.
-     * @return The User in case the application needs it.
-     * @throws exceptions.UserNotFoundException If the username is not found in the DB.
-     * @throws exceptions.PasswordDoesNotMatchException If the password does not 
-     * match with the username in the DB.
-     * @throws exceptions.UnexpectedErrorException If anything else goes wrong.
-     */
-    @Override
-    public synchronized User signIn(User user) throws UserNotFoundException, PasswordDoesNotMatchException, UnexpectedErrorException{
-        Message serverResponse = sendMessage(new Message(Message.Type.SIGN_IN, user));
-        switch(serverResponse.getType()) {
-        case SIGN_IN:
-            User logInUser = (User)serverResponse.getData();
-            return logInUser;
-        case USER_NOT_FOUND:
-            throw new UserNotFoundException(user.getLogin());
-        case PASSWORD_DOES_NOT_MATCH:
-            throw new PasswordDoesNotMatchException();
-        default:
-            throw new UnexpectedErrorException();
-        }
+    public User signIn(User user) throws UserNotFoundException, PasswordDoesNotMatchException, UnexpectedErrorException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
