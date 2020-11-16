be8ee38af81eec4c71cc1c94ed82b20ff3cbdcc0
grupo2i <72964155+grupo2i@users.noreply.github.com>
2020-11-16 12:20:42 +0100

    Update LogOutController.java

--- src/view/LogOutController.java
+++ src/view/LogOutController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import user.User;
 
 /**
@@ -34,17 +35,28 @@ public class LogOutController {
 
     /**
      * Switches to the Log in window.
-     *
+     * And the alert confirmation is showed
      * @param event
      */
     @FXML
     public void handleButtonLogOut(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogInWindow.fxml"));
-            Parent root = (Parent) loader.load();
-            LogInController controller = (loader.getController());
-            controller.setStage(stage);
-            controller.initStage(root);
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText(null);
+            alert.setTitle("Confirmación");
+            alert.setContentText("¿Estas seguro de que quieres salir?");
+            Optional<ButtonType> action = alert.showAndWait();
+            if (action.get() != ButtonType.OK) {
+                this.btnExit.setText("Cancelamos");
+            } else {
+                this.btnExit.setText("Adiós");
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogInWindow.fxml"));
+                Parent root = (Parent) loader.load();
+                LogInController controller = (loader.getController());
+                controller.setStage(stage);
+                controller.initStage(root);
+            }
+
         } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Could not change to Sign In window.", ButtonType.OK);
             alert.showAndWait();
@@ -52,7 +64,7 @@ public class LogOutController {
     }
 
     /**
-     * Closes application.
+     * Closes application with the alert confirmation.
      *
      * @param event
      */
@@ -63,11 +75,11 @@ public class LogOutController {
         alert.setTitle("Confirmación");
         alert.setContentText("¿Estas seguro de que quieres salir?");
         Optional<ButtonType> action = alert.showAndWait();
-        if (action.get() == ButtonType.OK) {
+        if (action.get() != ButtonType.OK) {
+            this.btnExit.setText("Cancelamos");
+        } else {
             this.btnExit.setText("Adiós");
             stage.close();
-        } else {
-            this.btnExit.setText("Cancelamos");
         }
     }
 
@@ -102,12 +114,14 @@ public class LogOutController {
      * Initializes the scene and its components
      *
      * @param root
+     * @param user
      */
-    public void initStage(Parent root) {
+    public void initStage(Parent root, String user) {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Log Out");
         stage.setResizable(false);
+        stage.setOnCloseRequest((event) -> this.exitApplication(event));
         lblGreeting.setText("Hola " + user);
         btnLogOut.setTooltip(
                 new Tooltip("Click to log out "));
@@ -116,6 +130,34 @@ public class LogOutController {
         btnLogOut.setDefaultButton(true);
         stage.show();
 
+    }
+
+    /**
+     * Initializates the Stage
+     *
+     * @param root
+     */
+    void initStage(Parent root) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Put the confirmation alert on the button exit of the application
+     *
+     * @param event
+     */
+    private void exitApplication(WindowEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Confirmación");
+        alert.setContentText("¿Estas seguro de que quieres salir?");
+        Optional<ButtonType> action = alert.showAndWait();
+        if (action.get() != ButtonType.OK) {
+            this.btnExit.setText("Cancelamos");
+        } else {
+            this.btnExit.setText("Adiós");
+            stage.close();
+        }
     }
 
 }
